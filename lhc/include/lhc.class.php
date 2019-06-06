2e37eb0579ec2e867c6c846d18242f20d932bdf3
yurisa2-i3wm <yuri@sa2.com.br>
2019-06-06 14:45:41 -0300

    Initial

---
+++ lhc/include/lhc.class.php
@@ -0,0 +1,28 @@
+<?php
+
+class lhc {
+  public function __construct() 
+  {
+    
+  }
+  
+  public function check_open_chat($phone) {
+  
+    return $chat_id;
+  }
+  
+  public function start_new_chat($phone) {
+
+    return $chat_id;
+  }
+  
+  public function send_msg_lhc($phone,$msg) {
+    // Check if there's a openchat, if not, start new
+  }
+  
+  
+  
+}
+
+
+?>
---
+++ waboxapp/include/wapp.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class wapp {
+  public function __construct() 
+  {
+    
+  }
+  
+  public function send_msg_wapp($phone,$msg) {
+    
+  }
+  
+  
+}
+
+
+?>
