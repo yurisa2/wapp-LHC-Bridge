b499edc4649f375759a11e24648f6cc96df6d11a
yurisa2-i3wm <yuri@sa2.com.br>
2019-06-06 18:36:21 -0300

    Waboxapp fields

--- lhc/lhchook.php
+++ lhc/lhchook.php
@@ -5,12 +5,29 @@ include 'include/lhrestapi.php';
 include 'include/lhc.config.php';
 include 'include/lhc.class.php';
 
-$from_phone = $_POST["from_phone"];
-$from_name = $_POST["from_name"];
-$msg = $_POST["msg"];
+// "event": "message",
+// "token": "abcd1234",
+// "contact[uid]": "34666123456",
+// "contact[name]": "Peter",
+// "contact[type]": "user",
+// "message[dtm]": 1487082303,
+// "message[uid]": "62397B58E3E0B",
+// "message[cuid]": "",
+// "message[dir]": "i",
+// "message[type]": "chat",
+// "message[body][text]": "Hey! How are you doing?",
+// "message[ack]": 3
 
-$lhc = new lhc;
+if($_POST["event"] == "message")
+{
+  $from_phone = $_POST["contact[uid]"];
+  $from_name = $_POST["contact[name]"];
+  $msg = $_POST["message[body][text]"];
+  
+  $lhc = new lhc;
+  
+  $lhc->send_msg_lhc($from_phone,$from_name,$msg);
+}
 
-$lhc->send_msg_lhc($from_phone,$from_name,$msg);
 
 ?>
