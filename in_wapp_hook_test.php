d3d3f83348889fb3d16a817ef0d851410d5fd309
yurisa2-i3wm <yuri@sa2.com.br>
2019-06-08 15:39:35 -0300

    Debug 1

--- in_wapp_hook_test.php
+++ in_wapp_hook_test.php
@@ -5,11 +5,11 @@ include 'lhc/include/lhc.config.php';
 include 'lhc/include/lhrestapi.php';
 include 'lhc/include/lhc.class.php';
 
-$json = file_get_contents('php://input');
+$incoming = $_POST;
 
 // file_put_contents("in.wapp_hook-json.json",$json);
 
-include 'wapi/wapihook.php';
+// include 'wapi/wapihook.php';
 
 file_put_contents("in.wapp_hook-incoming.json",json_encode($incoming));
 
