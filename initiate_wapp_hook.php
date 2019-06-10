00d9f8b20d48d0e1466f9eb5440505e5c0c2a6cf
yurisa2-i3wm <yuri@sa2.com.br>
2019-06-10 02:57:20 -0300

    Initiate Wap

---
+++ initiate_wapp_hook.php
@@ -0,0 +1,48 @@
+<?php
+ini_set("display_errors","on");
+include 'wapi/include/wapi.config.php';
+include 'wapi/include/vendor/autoload.php';
+include 'wapi/include/wapi.class.php';
+
+// FOR ERROR REPORTING
+
+include 'lhc/include/lhc.config.php';
+include 'lhc/include/lhrestapi.php';
+include 'lhc/include/lhc.class.php';
+
+
+
+$from_phpr = $_POST;
+
+$from_phpr = json_decode($from_phpr['params']);
+
+$phone = $from_phpr->phone."@s.whatsapp.net";
+
+$wapp = new wapi($config_wapi);
+$send = $wapp->send_msg_wapi($phone,$from_phpr->message);
+
+
+$dbh = new PDO('mysql:host='.$s.';dbname='.$d, $u, $p);
+$sql = "SELECT * FROM bookmarks where phone = '$from_phpr->phone'";
+  
+$sth = $dbh->prepare($sql);
+$sth->execute();
+
+$result = $sth->fetch(PDO::FETCH_ASSOC);
+ 
+// file_put_contents("res-mysql.json",serialize($result));
+// file_put_contents("res-sql.json",json_encode($sql));
+// file_put_contents("res-name.json",$result["name"]);
+
+if($result["name"] == NULL) $result["name"] = "Cliente";
+
+
+$message_to_lhc = "[i] Contato iniciado ativamente:  [/i] | Mensagem Inicial: ".$from_phpr->message;
+
+$lhc = new lhc($config_lhc);
+$lhc->send_msg_lhc($phone,$result["name"],$message_to_lhc);
+
+
+
+
+?>
