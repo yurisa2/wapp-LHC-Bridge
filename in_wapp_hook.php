6013b116406726cd46b8ffd06b531d42604dd649
yurisa2-i3wm <yuri@sa2.com.br>
2019-06-09 23:11:10 -0300

    if on

--- in_wapp_hook.php
+++ in_wapp_hook.php
@@ -19,24 +19,24 @@ if($incoming["from_me"] == 0) {
   $arr = explode("@", $incoming["user_from"]);
   $phone_only = $arr[0];  
   
-$dbh = new PDO('mysql:host='.$s.';dbname='.$d, $u, $p);
-$sql = "SELECT * FROM bookmarks where phone = '$phone_only'";
+  $dbh = new PDO('mysql:host='.$s.';dbname='.$d, $u, $p);
+  $sql = "SELECT * FROM bookmarks where phone = '$phone_only'";
+    
+  $sth = $dbh->prepare($sql);
+  $sth->execute();
+
+  $result = $sth->fetch(PDO::FETCH_ASSOC);
+   
+  // file_put_contents("res-mysql.json",serialize($result));
+  // file_put_contents("res-sql.json",json_encode($sql));
+  // file_put_contents("res-name.json",$result["name"]);
   
-$sth = $dbh->prepare($sql);
-$sth->execute();
-
-
-
-$result = $sth->fetch(PDO::FETCH_ASSOC);
-  
-  file_put_contents("res-mysql.json",serialize($result));
-  file_put_contents("res-sql.json",json_encode($sql));
-  file_put_contents("res-name.json",$result["name"]);
+  if($result["name"] == NULL) $result["name"] = "Cliente";
   
   $lhc = new lhc($config_lhc);
-  $lhc->send_msg_lhc($incoming["user_from"],$incoming["user_from"],$incoming["message_body"]);
+  $lhc->send_msg_lhc($incoming["user_from"],$result["name"],$incoming["message_body"]);
   
-} //This was not supposed to be working, but I guess its denying from the other end.....whatever. Will try to activate to keep clean operation
+} 
 
 
 ?>
